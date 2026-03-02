a1c9698dd4fad33def19f56784e830d12d480e3e
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-03-02 16:27:06 +0100

    sphere-packing: add Verso blueprint port scaffold

---
+++ test-projects/Sphere-Packing-Lean/SpherePackingBlueprintMain.lean
@@ -0,0 +1,32 @@
+import Std.Data.HashMap
+import VersoManual
+import SpherePackingBlueprint.Contents
+
+open Verso Doc
+open Verso.Genre Manual
+open Std (HashMap)
+
+def htmlAssets : HtmlAssets where
+  extraCss := {}
+  extraJs := {}
+  extraJsFiles := {}
+  extraCssFiles := {}
+
+def htmlConfig : HtmlConfig where
+  toHtmlAssets := htmlAssets
+  htmlDepth := 1
+  extraHead : Array Output.Html := #[]
+
+def outputConfig : OutputConfig where
+  emitTeX := false
+  emitHtmlSingle := .no
+  emitHtmlMulti := .immediately
+
+def config : Config where
+  toHtmlConfig := htmlConfig
+  toOutputConfig := outputConfig
+
+def renderConfig : RenderConfig where
+  toConfig := config
+
+def main := manualMain (%doc SpherePackingBlueprint.Contents) (config := renderConfig)
