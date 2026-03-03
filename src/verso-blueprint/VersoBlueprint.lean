bbe5ad501dc5f41654459fef41b1c3f27b55d971
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-03-03 12:02:42 +0100

    refactor: extract graph renderer module

--- src/verso-blueprint/VersoBlueprint.lean
+++ src/verso-blueprint/VersoBlueprint.lean
@@ -18,6 +18,7 @@ import VersoBlueprint.Environment
 import VersoBlueprint.Attribute
 import VersoBlueprint.Cite
 import VersoBlueprint.Commands
+import VersoBlueprint.Commands.RenderGraph
 import VersoBlueprint.Commands.ShowGraph
 import VersoBlueprint.Commands.ShowSummary
 import VersoBlueprint.Commands.ShowBibliography
